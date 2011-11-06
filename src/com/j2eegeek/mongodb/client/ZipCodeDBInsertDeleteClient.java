b37d88d2578eb43361a880ecc822722a781f7f76
Vinny Carpenter <vscarpenter@gmail.com>
2011-11-05 22:24:50 -0500

    Updates to the example to use the findAndRemove and findAnyModify methods

--- src/com/j2eegeek/mongodb/client/ZipCodeDBInsertDeleteClient.java
+++ src/com/j2eegeek/mongodb/client/ZipCodeDBInsertDeleteClient.java
@@ -5,11 +5,11 @@ import com.mongodb.*;
 import java.net.UnknownHostException;
 
 /**
- * Created by IntelliJ IDEA.
- * User: vcarpenter
- * Date: 11/5/11
- * Time: 9:24 PM
- * To change this template use File | Settings | File Templates.
+ * The <code>ZipCodeDBInsertDeleteClient</code> class demonstrates some simple insert, update and delete
+ * functionality to modify documents in a collection.
+ *
+ * @author Vinny Carpenter (vscarpenter@nospam.gmail.com)
+ * @version 1.0
  */
 public class ZipCodeDBInsertDeleteClient {
 
@@ -38,12 +38,35 @@ public class ZipCodeDBInsertDeleteClient {
 
         DBCursor zipSearchCursor = dbCollection.find(zipQuery);
         while (zipSearchCursor.hasNext()) {
-            System.out.println("zipSearchCursor.next() = " + zipSearchCursor.next());
+            DBObject dbObject = zipSearchCursor.next();
+            System.out.println("dbObject.toString() = " + dbObject.toString());
         }
 
         zipSearchCursor.close();
 
+        //Let's update the record we just added
+        DBObject updatedObject = new BasicDBObject();
+        updatedObject.put("city", "VINNY-UPDATED-CITY");
+        updatedObject.put("zip", "99999");
+        updatedObject.put("loc", "");
+        updatedObject.put("pop", "123456");
+        updatedObject.put("state", "WI");
 
+        dbCollection.findAndModify(zipQuery, updatedObject);
+
+        //Let's search again for see if the update took.
+        DBCursor updatedResults = dbCollection.find(zipQuery);
+        while (updatedResults.hasNext()) {
+            System.out.println("Updated Results = " + updatedResults.next());
+        }
+
+        //now let's delete that document/record.
+        BasicDBObject itemsToDeleteQuery = new BasicDBObject("zip", "99999");
+        DBObject itemsBeingRemoved = dbCollection.findAndRemove(itemsToDeleteQuery);
+        System.out.println("Item being deleted = " + itemsBeingRemoved.toString());
+
+        //Close the underlying connection.
+        mongo.close();
 
     }
 }
